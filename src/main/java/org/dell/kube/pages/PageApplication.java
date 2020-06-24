a39f4c364ef9db678e5be78277038318f8b9984b
aditya-bhuyan <aditya.sunjava@gmail.com>
2020-06-24 18:30:12 +0530

    Hello App Start with test cases

--- src/main/java/org/dell/kube/pages/PageApplication.java
+++ src/main/java/org/dell/kube/pages/PageApplication.java
@@ -7,11 +7,6 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class PageApplication {
 
-	@Bean
-	public IPageRepository iPageRepository(){
-		return  new InMemeoryPageRepository();
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(PageApplication.class, args);
 	}
