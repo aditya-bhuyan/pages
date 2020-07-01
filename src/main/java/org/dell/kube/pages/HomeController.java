b004433f1384436b2cad0147cbb8db3fa12d11ac
aditya-bhuyan <aditya.sunjava@gmail.com>
2020-07-01 22:36:41 +0530

    persistance-solution

--- src/main/java/org/dell/kube/pages/HomeController.java
+++ src/main/java/org/dell/kube/pages/HomeController.java
@@ -1,14 +1,18 @@
 package org.dell.kube.pages;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 @RequestMapping("/")
 public class HomeController {
-
+    Logger loger = LoggerFactory.getLogger(HomeController.class);
     private String pageContent;
 
     public HomeController(@Value("${page.content}") String pageContent){
@@ -17,6 +21,11 @@ public class HomeController {
     }
     @GetMapping
     public String getPage(){
+        loger.error("Welcome Page Accessed");
+        loger.debug("Welcome Page Accessed");
+        loger.info("Welcome Page Accessed");
+        loger.trace("Welcome Page Accessed");
+        loger.warn("Welcome Page Accessed");
         return "Hello from page : "+pageContent+" ";
     }
 
--- src/main/java/org/dell/kube/pages/PageApplication.java
+++ src/main/java/org/dell/kube/pages/PageApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PageApplication {
 
 	@Bean
-	public IPageRepository iPageRepository(){
-		return new InMemoryPageRepository();
+	public IPageRepository iPageRepository(DataSource dataSource){
+		return new MySqlPageRepository(dataSource);
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(PageApplication.class, args);
